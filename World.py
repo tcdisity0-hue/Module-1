13c6237b021c251d530e96d3e8946d58def414a1
tcdisity <tcdisity0@gmail.com>
2026-01-15 11:02:00 +0200

    Start

--- World.py
+++ World.py
@@ -1,6 +1,30 @@
 (print('Hello World!'))
 
+
 print('"Hello", said Joe')
 
-user = input('Enter your name: ')
+print('Hallo da Damen und Herren')
+
+name = input('Enter your name: ')
+print(name)
+
+Nu= int(input('Number 1'))
+Nu2= int(input('Number 2'))
+
+div=int(Nu)/int(Nu2)
+
+print(div)
+
+Nu3 = int(input('Number 3'))
+Nu4 = int(input('Number 4'))
+
+max=int(Nu3)*int(Nu4)
+print(max)
+
+Nu5 = int(input('Number 5'))
+Nu6 = int(input('Number 6'))
+
+add=int(Nu5)+int(Nu6)
+print(add)
+
 
